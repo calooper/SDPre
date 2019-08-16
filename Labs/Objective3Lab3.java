ee659ed38b3b34a1b5d5e14d27e8318902cc14ec
Colt <coltaustin45@hotmail.com>
2019-08-16 13:32:22 -0700

    objective 3 lab 3

---
+++ Labs/Objective3Lab3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Objective3Lab3 {
+  public static void main(String [] args ) {
+    Scanner input = new Scanner(System.in);
+
+    int currentYear = 2019, birthYear, age;
+
+    System.out.println( "How old are you?" );
+    age = input.nextInt();
+
+    birthYear = (currentYear - age);
+
+    System.out.println( "You were born in " + birthYear);
+    input.close();
+
+
+  }
+}
