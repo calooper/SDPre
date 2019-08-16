b5efa1842f7bea59a358040edd22961b6cf692f3
Colt <coltaustin45@hotmail.com>
2019-08-16 10:59:45 -0700

    objective 2 lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,13 @@
+public class Objective2Lab2 {
+  public static void main(String [] args ) {
+
+    String name = "Colt Looper", state = "Oklahoma";
+    int age = 28;
+
+    System.out.println( name + "\n" + state + "\n" + age );
+
+
+
+
+  }
+}
