b8fd84a3bcf678f3b838c2d8beb82eb09b80763a
Colt <coltaustin45@hotmail.com>
2019-08-16 13:08:00 -0700

    ex 09 BMI calculator

---
+++ JavaTheHardWay/BMICalculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class BMICalculator {
+    public static void main(String[] args) {
+      Scanner keyboard = new Scanner(System.in);
+
+      //declared variables
+      double m, kg, bmi;
+
+      System.out.println( "Your height in feet: " );
+      int feet = keyboard.nextInt();
+
+      System.out.println( "And inches? " );
+      int inches = keyboard.nextInt();
+
+      System.out.println( "Your weight in lbs: " );
+      int lbs = keyboard.nextInt();
+
+      //calcualtions
+      m  = (feet / 3.28084) + (inches * 0.0254);
+      kg = (lbs * 0.45359);
+      bmi = kg / (m * m);
+
+      System.out.println( "Your BMI is " + bmi );
+    }
+  }
