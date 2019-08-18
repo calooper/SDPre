f588038b81eb3c05d87baadb5f2bde153029127c
Colt <coltaustin45@hotmail.com>
2019-08-18 09:22:48 -0700

    age messages

--- JavaTheHardWay/AgeMessages.java
+++ JavaTheHardWay/AgeMessages.java
@@ -11,18 +11,22 @@ public class AgeMessages {
       System.out.println( "You are: ");
       if ( age < 13 )
         System.out.println( "\ttoo young to create a Facebook account " );
+
       if ( age < 16 )
         System.out.println( "\ttoo young to get a driver's license " );
+
       if ( age < 18 )
         System.out.println( "\ttoo young too get a tattoo " );
+
       if ( age < 21 )
         System.out.println( "\ttoo young to drink alcohol " );
+
       if (age < 35 ) {
         System.out.println( "\ttoo young to run for President of the U.S. " );
         System.out.println( "\t\t(How sad!)" );
       }
       if ( age >= 65 )
-        System.out.println( "\tyour are old enough to retire! ");
+        System.out.println( "\tyou're are old enough to retire! ");
 
     }
   }
