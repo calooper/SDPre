bfed75a7d5c9369219b7c237a56745b944e1e0e6
Colt <coltaustin45@hotmail.com>
2019-08-18 12:55:44 -0700

    objective 6 lab 1

---
+++ Labs/Objective6Lab1.java
@@ -0,0 +1,8 @@
+public class Objective6Lab1 {
+    public static void main(String[] args) {
+
+      for (int counter = 10; counter >= 0; counter --) {
+      System.out.println( counter );
+    }
+  }
+}
